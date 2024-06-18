1f6a060204f32b2751391bf95c5cb03466dec2a2
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-06-18 07:26:17 +0300

    fastapi updated

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
+import time
 from fastapi import FastAPI
 app = FastAPI()
-@app.get("/")
+from datetime import datetime
+
+@app.get("/temperature")
 def read_root():
-    response = requests.get("https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json")
-    response = response.json()['sensors'][2]['lastMeasurement']['value']
+    now = datetime.now()
+    now = str(now.time())
+    clock_hour_now = int(now[1])
+    response1 = requests.get("https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json")
+    response1 = float(response1.json()['sensors'][2]['lastMeasurement']['value'])
+    response2 = requests.get("https://api.opensensemap.org/boxes/5eba5fbad46fb8001b799786?format=json")
+    time_of_mesurement = response2.json()['sensors'][2]['lastMeasurement']['createdAt']
+    response2 = float(response2.json()['sensors'][2]['lastMeasurement']['value'])
+    response3 = requests.get("https://api.opensensemap.org/boxes/5eb99cacd46fb8001b2ce04c?format=json")
+    response3 = float(response3.json()['sensors'][1]['lastMeasurement']['value'])
+    response = (response1 + response2 + response3) // 3
+    difference_in_time_between_mesurement_now = clock_hour_now - int(time_of_mesurement[12])
     print(response)
-    print(type(response))
-    return {response}
-
-#for index in req :
-
-
+    return response, f'difference in time between mesurement now (hours) = {difference_in_time_between_mesurement_now}'
+@app.get("/version")
 def print_version():
-    print("current version is v0.0.1")
+    return "current version is v0.0.1"
 
 
-print_version()
+
