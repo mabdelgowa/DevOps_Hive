a183417f9f27d8a07dfe064d72b40ec833ea718b
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-06-18 04:45:06 +0300

    fastapi updated

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import requests
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def read_root():
+    response = requests.get("https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json")
+    response = response.json()['sensors'][2]['lastMeasurement']['value']
+    print(response)
+    print(type(response))
+    return {response}
+
+#for index in req :
+
+
 def print_version():
     print("current version is v0.0.1")
 
