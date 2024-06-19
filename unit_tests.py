5402fdae6490e9e4105fe3651af2da72c38ed6cd
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-06-19 03:39:54 +0300

    pytest_for_unittests

---
+++ unit_tests.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from main import app
+from fastapi import status
+
+
+client = TestClient(app=app)
+
+def test_temp():
+    response = client.get('/temperature')
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_ver():
+    response = client.get('/version')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.text == '"current version is v0.0.1"'
