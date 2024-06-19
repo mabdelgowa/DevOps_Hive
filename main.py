edda5c9f3259cfdaf9f850c83278bc42f1d3d65a
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-06-20 00:37:42 +0300

    phase4_update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
-
-from fastapi import FastAPI
+import prometheus_client
+from fastapi import FastAPI, Response
 app = FastAPI()
 from datetime import datetime
 
@@ -17,12 +17,26 @@ def read_root():
     response3 = requests.get("https://api.opensensemap.org/boxes/5eb99cacd46fb8001b2ce04c?format=json")
     response3 = float(response3.json()['sensors'][1]['lastMeasurement']['value'])
     response = (response1 + response2 + response3) // 3
+    if response < 10:
+        status = "Too_cold"
+    elif response > 11 and response < 36:
+        status = "good"
+    else:
+        status = "Too_hot"
+
     difference_in_time_between_mesurement_now = clock_hour_now - int(time_of_mesurement[12])
     print(response)
-    return response, f'difference in time between mesurement now (hours) = {difference_in_time_between_mesurement_now}'
+    return response, f'difference in time between mesurement now (hours) = {difference_in_time_between_mesurement_now}', status
 @app.get("/version")
 def print_version():
     return "current version is v0.0.1"
+
+@app.get("/metrics")
+def send_metrics():
+    return Response(
+        media_type="text/plain",
+        content=prometheus_client.generate_latest(),
+    )
 
 
 
