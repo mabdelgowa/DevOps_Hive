b815ec70af71701cbc5dd86647b645dcac09bc2d
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-06-23 15:11:34 +0300

    phase5_update

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from fastapi.testclient import TestClient
 app = FastAPI()
 from datetime import datetime
 
+c = prometheus_client.Counter('my_response_total', 'HTTP Failures', ['method', 'endpoint'])
+
 @app.get("/temperature")
 def read_root():
     now = datetime.now()
     now = str(now.time())
     clock_hour_now = int(now[1])
     response1 = requests.get("https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json")
+    if response1.status_code == 200:
+        c.labels('get', 'https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json').inc()
     response1 = float(response1.json()['sensors'][2]['lastMeasurement']['value'])
     response2 = requests.get("https://api.opensensemap.org/boxes/5eba5fbad46fb8001b799786?format=json")
+    if response2.status_code == 200:
+        c.labels('get', 'https://api.opensensemap.org/boxes/5eba5fbad46fb8001b799786?format=json').inc()
     time_of_mesurement = response2.json()['sensors'][2]['lastMeasurement']['createdAt']
     response2 = float(response2.json()['sensors'][2]['lastMeasurement']['value'])
     response3 = requests.get("https://api.opensensemap.org/boxes/5eb99cacd46fb8001b2ce04c?format=json")
+    if response3.status_code == 200:
+        c.labels('get', 'https://api.opensensemap.org/boxes/5eb99cacd46fb8001b2ce04c?format=json').inc()
     response3 = float(response3.json()['sensors'][1]['lastMeasurement']['value'])
+
+
     response = (response1 + response2 + response3) // 3
     if response < 10:
         status = "Too_cold"
@@ -28,10 +38,12 @@ def read_root():
     difference_in_time_between_mesurement_now = clock_hour_now - int(time_of_mesurement[12])
     print(response)
     return response, f'difference in time between mesurement now (hours) = {difference_in_time_between_mesurement_now}', status
+
+
+
 @app.get("/version")
 def print_version():
     return "current version is v0.3.0"
-
 @app.get("/metrics")
 def send_metrics():
     return Response(
@@ -40,6 +52,21 @@ def send_metrics():
     )
 
 
+
+@app.get("/readyz")
+def readyz():
+    fastapi.ca
+    response1 = requests.get("https://api.opensensemap.org/boxes/5e60cf5557703e001bdae7f8?format=json")
+    response2 = requests.get("https://api.opensensemap.org/boxes/5eba5fbad46fb8001b799786?format=json")
+    response3 = requests.get("https://api.opensensemap.org/boxes/5eb99cacd46fb8001b2ce04c?format=json")
+    if str(response1) != "<Response [200]>" and str(response2) != "<Response [200]>" :
+        return "HTTP != 200"
+    elif str(response2) != "<Response [200]>" and str(response3) != "<Response [200]>" :
+        return "HTTP != 200"
+    elif str(response1) != "<Response [200]>" and str(response3) != "<Response [200]>" :
+        return "HTTP != 200"
+    else:
+        return "HTTP = 200"
 
 client = TestClient(app)
 def test_read_main():
