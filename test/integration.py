041c72f2de360f72432b8168597761a9b5445a7b
alexGradProj2023 <safety.box.graduation.project@gmail.com>
2024-12-22 16:48:24 +0200

    KAN-13 editing integratin tests to import main file

--- test/integration.py
+++ test/integration.py
@@ -1,16 +1,16 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-from main import app
+from context import main
 ##########################This part for integration test to test all pathes at one time########################
 
-client = TestClient(app)
+client = TestClient(main.app)
 def test_read_main():
-    response = client.get("http://127.0.0.1/temperature")
+    response = client.get("/temperature")
     assert response.status_code == 200
-    response = client.get("http://127.0.0.1/version")
+    response = client.get("/version")
     assert response.status_code == status.HTTP_200_OK
     assert response.text == '"current version is v0.3.0"'
-    response = client.get("http://127.0.0.1/metrics")
+    response = client.get("/metrics")
     assert response.status_code == status.HTTP_200_OK
 
 
